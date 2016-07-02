94aea13d04a1b960241933098abadd4d0e8e03e4
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-07-02 08:02:33 -0500

    getting basic server going

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(PORT, () => {
+  console.log(`Node.js server started. Listening on port ${PORT}`);
+});
+
