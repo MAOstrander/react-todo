babd8ee2ef2578e6c40f342eec22ceec31e5b976
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-07-06 14:38:20 -0500

    clearing out console logs

--- server.js
+++ server.js
@@ -19,13 +19,10 @@ const MONGODB_URL_PREFIX = MONGODB_USER
 //mongodb://<dbuser>:<dbpassword>@ds051575.mlab.com:51575/todo
 const MONGO_URL = `mongodb://${MONGODB_URL_PREFIX}${MONGODB_HOST}:${MONGODB_PORT}/todo`;
 
-// app.set('view engine', 'html');
-// app.use(express.static('public'));
-
 app.use(bodyParser.urlencoded( {extended: false} ));
 app.use(bodyParser.json() );
-
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static('public'));
 
 
 // Break this out into a new file soon
@@ -89,7 +86,6 @@ app.post('/api/edit', (req, res) => {
 
 app.delete('/api/delete', (req, res) => {
   let searchID = req.body.idToDelete;
-  console.log("ID TO REMOVE", req.body.idToDelete);
   Task.remove( {"_id": searchID}, (err, doc) => {
     if (err) throw err;
 
@@ -105,6 +101,6 @@ mongoose.connect(MONGO_URL);
 mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
 mongoose.connection.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`Node.js server started. Listening on port ${PORT}. Supposedly connected to database`);
+    console.log(`Node.js server started. Listening on port ${PORT}.`);
   });
 });
